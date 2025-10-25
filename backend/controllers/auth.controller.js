cefd2a3c0a86ef3ba8df282910226ce9632539c7
Paula-Jagic <pj55077@fer.hr>
2025-10-26 00:43:49 +0200

    Remove auth.controller.js - using express-openid-connect instead

--- backend/controllers/auth.controller.js
+++
@@ -1,9 +0,0 @@
-export const login = (req, res) => {
-  res.oidc.login({ returnTo: "/home" });
-};
-
-export const callback = (req, res) => {
-  console.log(req.oidc.user); 
-  res.redirect("http://localhost:5173/home"); 
-};
-
