2ae5d918b45f91b1da210c42f48306fcb02c15b2
Paula-Jagic <pj55077@fer.hr>
2025-10-26 20:04:48 +0100

    3fila

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,28 @@
-
-import LoginPage from "./pages/LoginPage";
-import HomePage from "./pages/HomePage";
-import TicketPage from './pages/TicketPage';
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+// App.jsx
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute'; // 👈 UVEZITE PRIVATE ROUTE
+import LoginPage from './LoginPage';
+import HomePage from './HomePage';
+import TicketPage from './TicketPage';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/ticket/:ticketId" element={<TicketPage />} />
-        
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                {/* 1. JAVNA RUTA: Login Page (osnovni URL) */}
+                <Route path="/" element={<LoginPage />} />
+
+                {/* 2. POTPUNO JAVNA RUTA: TICKET PAGE 
+                   🛑 OVA RUTA JE IZUZETA OD SVIH AUTH PROVJERA */}
+                <Route path="/ticket/:ticketId" element={<TicketPage />} />
+
+                {/* 3. ZAŠTIĆENA RUTA: Home Page 
+                   🛑 OVDJE SE AKTIVIRA AUTH0 PROVJERA/REDIREKCIJA */}
+                <Route path="/home" element={<PrivateRoute element={HomePage} />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
