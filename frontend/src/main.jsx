94d481694f304258de1e2b7974462d6853e566fa
Paula-Jagic <pj55077@fer.hr>
2025-10-26 20:15:20 +0100

    3fila

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,25 +1,11 @@
-// main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Auth0Provider } from '@auth0/auth0-react'; // 👈 UVEZITE OVO
+
 import './App.css'
 import App from './App.jsx'
 
-// Definirajte svoje Auth0 varijable iz .env
-const domain = import.meta.env.VITE_AUTH0_DOMAIN;
-const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* 🛑 OMOTAJTE APLIKACIJU U AUTH0 PROVIDER */}
-    <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      authorizationParams={{
-        redirect_uri: window.location.origin
-      }}
-    >
-        <App />
-    </Auth0Provider>
+    <App />
   </StrictMode>,
-);
+)
