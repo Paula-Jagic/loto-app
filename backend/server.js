b1d7b3d55ec3964e5700ba4f8f07a258d4fb087e
Paula-Jagic <pj55077@fer.hr>
2025-10-26 02:36:26 +0200

    server

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(session({
   cookie: { 
     secure: true, // MORA biti true u produkciji
     httpOnly: true,
-    sameSite: 'lax', // Probaj 'lax' umjesto 'none'
+    sameSite: 'none', // Probaj 'lax' umjesto 'none'
     maxAge: 24 * 60 * 60 * 1000
   }
 }));
